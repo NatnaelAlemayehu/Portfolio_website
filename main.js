3bc4561268073dea1d6f4f106977f7aafd1091e7
Your Name <you@example.com>
2020-03-05 17:40:37 +0200

    files

---
+++ main.js
@@ -0,0 +1,9 @@
+function open_Nav() {
+    document.getElementById("side_nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function close_Nav() {
+    document.getElementById("side_nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
